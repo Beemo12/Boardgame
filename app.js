1fd241f8777463e721e3be267dfb7c47f6ac7993
Yassine B <yassinemens53@gmail.com>
2023-11-24 15:07:55 +0100

    bord tekenen (nog niet af)

--- app.js
+++ app.js
@@ -11,6 +11,67 @@ let pawnpositions= [];
 let boardpositions= [];
 let playeramountbuttons= [];
 
+function createBoardPositions()
+{
+    let x= 0;
+    let y = canvas.height-boardPositionSize;
+    let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1] ;
 
+    for(let i =0 ; i<path.length;i++)
+    {
+
+        if(path[i] == 1)//gaan naar rechts
+        {
+            //bedenk hier wat je met de x moet doen
+            +10
+        }
+        else if(path[i] == 3)//gaan naar links
+        {
+            // bedenk hier wat je met de x moet doen
+            +10
+        }
+        else if(path[i] == 0)//gaan hier naar boven
+        {
+            //bedenk hier wat je met de y moet doen
+            -10
+        }
+        boardPositions.push(createRect(x,y,boardPositionSize,boardPositionSize));
+    }
+} 
+
+function draw()
+{
+    clearCanvas();
+    for(let i =0 ; i<boardPositions.length;i++)
+    {
+        let pos = boardPositions[i];
+
+        g.fillStyle  = "#004400";
+        g.fillRect(pos.x,pos.y,-10,+10);
+        g.fillStyle  = "#FFFFFF";
+        g.fillText((i+1)+"",pos.x,pos.y+20);
+    }
+}
+
+function clearCanvas()
+{
+    g.fillstyle = "cadetblue"
+    g.fillrect(0,0, canvas.width, canvas.height );
+}
+
+function createRect(x,y,w,h)
+{
+    let rectangle = {
+        x:x,
+        y:y,
+        x2:x+w,
+        y2:y+h,
+        w:w,
+        h:h,
+    };
+}
 let canvas = document.getElementById("canvas");
 let g = canvas.getContext("2d");
+
+createBoardPositions();
+draw();
