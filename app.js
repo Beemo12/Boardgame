b8ee576b5684c5dc20afcc8be03e26c6afadd348
Yassine B <yassinemens53@gmail.com>
2023-11-24 15:31:48 +0100

    bord tekenen

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ingamestate_start=0;
 const ingamestate_roll=1;
 const ingamestate_end=0;
 
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d");
+
 let boardpositionsize=50;
 let pawnpositions= [];
 let boardpositions= [];
@@ -14,7 +17,7 @@ let playeramountbuttons= [];
 function createBoardPositions()
 {
     let x= 0;
-    let y = canvas.height-boardPositionSize;
+    let y = canvas.height-boardpositionsize;
     let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1] ;
 
     for(let i =0 ; i<path.length;i++)
@@ -23,31 +26,31 @@ function createBoardPositions()
         if(path[i] == 1)//gaan naar rechts
         {
             //bedenk hier wat je met de x moet doen
-            +10
+            x+=boardpositionsize
         }
         else if(path[i] == 3)//gaan naar links
         {
             // bedenk hier wat je met de x moet doen
-            +10
+            x-=boardpositionsize
         }
         else if(path[i] == 0)//gaan hier naar boven
         {
             //bedenk hier wat je met de y moet doen
-            -10
+            y-=boardpositionsize
         }
-        boardPositions.push(createRect(x,y,boardPositionSize,boardPositionSize));
+        boardpositions.push(createRect(x,y,boardpositionsize,boardpositionsize));
     }
 } 
 
 function draw()
 {
     clearCanvas();
-    for(let i =0 ; i<boardPositions.length;i++)
+    for(let i =0 ; i<boardpositions.length;i++)
     {
-        let pos = boardPositions[i];
+        let pos = boardpositions[i];
 
         g.fillStyle  = "#004400";
-        g.fillRect(pos.x,pos.y,-10,+10);
+        g.fillRect(pos.x,pos.y,boardpositionsize,boardpositionsize);
         g.fillStyle  = "#FFFFFF";
         g.fillText((i+1)+"",pos.x,pos.y+20);
     }
@@ -56,7 +59,7 @@ function draw()
 function clearCanvas()
 {
     g.fillstyle = "cadetblue"
-    g.fillrect(0,0, canvas.width, canvas.height );
+    g.fillRect(0,0, canvas.width, canvas.height );
 }
 
 function createRect(x,y,w,h)
@@ -69,9 +72,8 @@ function createRect(x,y,w,h)
         w:w,
         h:h,
     };
+    return rectangle;
 }
-let canvas = document.getElementById("canvas");
-let g = canvas.getContext("2d");
 
 createBoardPositions();
 draw();
