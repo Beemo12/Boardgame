0f60da3b48609f9618d3331a1263cebfb3365a6f
Yassine B <yassinemens53@gmail.com>
2023-11-24 14:45:52 +0100

    variablen

---
+++ app.js
@@ -0,0 +1,16 @@
+const game_start=0;
+const gamestate_ingame=1;
+const gamestate_gameover=2;
+
+const ingamestate_start=0;
+const ingamestate_roll=1;
+const ingamestate_end=0;
+
+let boardpositionsize=50;
+let pawnpositions= [];
+let boardpositions= [];
+let playeramountbuttons= [];
+
+
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d");
